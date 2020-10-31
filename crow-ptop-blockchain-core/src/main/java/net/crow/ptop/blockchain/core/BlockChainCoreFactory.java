0a86db8779fe528a2fee88ac230df4a7df349a28
chennj <chennj_cn@aliyun.com>
2020-10-31 10:19:57 +0800

    update

--- crow-ptop-blockchain-core/src/main/java/net/crow/ptop/blockchain/core/BlockChainCoreFactory.java
+++ crow-ptop-blockchain-core/src/main/java/net/crow/ptop/blockchain/core/BlockChainCoreFactory.java
@@ -30,18 +30,21 @@ public class BlockChainCoreFactory {
      */
     public BlockChainCore createBlockChainCore(String blockchainDataPath,String minerAddress) throws Exception {
 
+    	// 建立矿工缺省激励机制
         Incentive incentive = new IncentiveDefaultImpl();
+        // 建立缺省共识机制
         Consensus consensus = new ProofOfWorkConsensusImpl();
         BlockChainDataBase blockChainDataBase = new BlockChainDataBaseDefaultImpl(blockchainDataPath,incentive,consensus);
-
+        // 初始化矿工对象实例
         MinerTransactionDtoDataBase minerTransactionDtoDataBase = new MinerTransactionDtoDtoDataBaseDefaultImpl(blockchainDataPath);
         StringAddress minerStringAddress = new StringAddress(minerAddress);
         Miner miner = new MinerDefaultImpl(blockChainDataBase, minerTransactionDtoDataBase,minerStringAddress);
-
+        // 初始化同步节点数据库对象实例
         SynchronizerDataBase synchronizerDataBase = new SynchronizerDataBaseDefaultImpl(blockchainDataPath);
+        // 初始化同步区块对象实例
         BlockChainDataBase temporaryBlockChainDataBase = new BlockChainDataBaseDefaultImpl(new File(blockchainDataPath,"TemporaryBlockChainDataBase").getAbsolutePath(),incentive,consensus);
         Synchronizer synchronizer = new SynchronizerDefaultImpl(blockChainDataBase,temporaryBlockChainDataBase, synchronizerDataBase);
-
+        // 初始化区块链核心对象
         BlockChainCore blockChainCore = new BlockChainCoreImpl(blockChainDataBase,miner,synchronizer);
         return blockChainCore;
     }
